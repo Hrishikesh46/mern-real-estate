6b76170c544b32f9e544d45641b8e38de3fd86db
Hrishi <hrishikeshyadav92@gmail.com>
2023-11-15 09:25:09 +0530

    add sign out functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -72,15 +72,16 @@ exports.google = async (req, res, next) => {
       const hashedPassword = bcrypt.hashSync(generatedPassword, 10);
       const newUser = await User.create({
         username:
-          req.body.name.split(" ").join("").toLowerCase() +
+          req.body.username.split(" ").join("").toLowerCase() +
           Math.random.toString(36).slice(-4),
         email: req.body.email,
         password: hashedPassword,
-        avatar: req.body.photo,
+        avatar: req.body.avatar,
       });
 
       const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET);
       const { password: pass, ...rest } = newUser._doc;
+      // console.log(rest);
 
       res
         .cookie("access_token", token, { httpOnly: true })
@@ -89,5 +90,14 @@ exports.google = async (req, res, next) => {
     }
   } catch (err) {
     next(err);
+  }
+};
+
+exports.signout = async (req, res, next) => {
+  try {
+    res.clearCookie("access_token");
+    res.status(200).json("User has been logged out!");
+  } catch (error) {
+    next(error);
   }
 };
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -13,6 +13,9 @@ import {
   deleteUserFailure,
   deleteUserStart,
   deleteUserSuccess,
+  signOutUserStart,
+  signOutUserFailure,
+  signOutUserSuccess,
 } from "../redux/user/userSlice";
 import { useDispatch } from "react-redux";
 import { app } from "../firebase";
@@ -111,6 +114,21 @@ function Profile() {
     }
   };
 
+  const handleSignOut = async () => {
+    try {
+      dispatch(signOutUserStart());
+      const res = await fetch("api/auth/signout");
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(signOutUserFailure());
+        return;
+      }
+      dispatch(signOutUserSuccess());
+    } catch (error) {
+      dispatch(signOutUserFailure());
+    }
+  };
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
@@ -176,7 +194,9 @@ function Profile() {
         >
           Delete Account
         </span>
-        <span className="text-red-700 cursor-pointer">Sign Out</span>
+        <span onClick={handleSignOut} className="text-red-700 cursor-pointer">
+          Sign Out
+        </span>
       </div>
 
       <p className="text-red-700 mt-5">{error ? error : ""}</p>
