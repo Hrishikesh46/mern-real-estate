e77218bbab8ea31204929ce8c6739621e6ced383
Hrishi <hrishikeshyadav92@gmail.com>
2023-11-22 12:30:50 +0530

    add pre installation

--- api/index.js
+++ api/index.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-dotenv.config();
 const userRouter = require("./routes/user.route");
 const authRouter = require("./routes/auth.route");
 const listingRouter = require("./routes/listing.route");
 const cookieParser = require("cookie-parser");
+const path = require("path");
+dotenv.config();
 
 mongoose
   .connect(process.env.MONGO)
@@ -21,6 +22,8 @@ app.listen(8000, () => {
   console.log("Listening on Port 8000  !");
 });
 
+const __dirname = path.resolve();
+
 // to get the req.body
 app.use(express.json());
 
@@ -29,6 +32,12 @@ app.use(cookieParser());
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 // to handle global error
 app.use((err, req, res, next) => {
