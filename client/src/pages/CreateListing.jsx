9e6c86ccccf520a47e471610a173b7a1bd1805ee
Hrishi <hrishikeshyadav92@gmail.com>
2023-11-22 11:47:07 +0530

    complete home page

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -300,7 +300,7 @@ function CreateListing() {
               <div className="flex flex-col items-center">
                 <p>Regular price</p>
                 {formData.type === "rent" && (
-                  <span className="text-xs">($ / month)</span>
+                  <span className="text-xs">(₹ / month)</span>
                 )}
               </div>
             </div>
@@ -319,7 +319,7 @@ function CreateListing() {
                 <div className="flex flex-col items-center">
                   <p>Discounted price</p>
                   {formData.type === "rent" && (
-                    <span className="text-xs">($ / month)</span>
+                    <span className="text-xs">(₹ / month)</span>
                   )}
                 </div>
               </div>
